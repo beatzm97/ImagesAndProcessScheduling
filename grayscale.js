40cb1d78c1203b10e9633c89e9fc8c82951c0e12
beatzm97 <bmanrique@smu.edu>
2018-04-24 19:16:12 -0500

    GrayScale finished

--- grayscale.js
+++ grayscale.js
@@ -2,8 +2,13 @@
 // CSE 3353
 // Project 3: Images and Process Scheduling
 
-var Jimp = require("jimp");
+var Jimp = require("jimp"); // read in jimp
 
+// grayscales input image
+// fileTypes supported include .jpg .png .bmp
+// scans image and reads Red, Green, and Blue values of each pixel
+// averages out the read values and reassigns each RGB to the averaged value
+// returns grayscaled image as a new image called imageGray with matching filetype and input
 function grayScale (imageName, fileType){
     Jimp.read(imageName+"."+fileType).then(function(image){
         image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, index){
@@ -16,7 +21,7 @@ function grayScale (imageName, fileType){
             this.bitmap.data[index+2] = gray;
             var alpha = this.bitmap.data[index+3];
         })
-        return image.write("imageGray.jpg"); // save
+        return image.write("imageGray."+fileType); // save
     }).catch(function (err) {
         console.error(err);
     })
